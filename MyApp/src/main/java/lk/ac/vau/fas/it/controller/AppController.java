7d6009acdcd4b390930805a4026c656cf59e0afa
madushilakmini <139555956+madushilakmini@users.noreply.github.com>
2025-03-30 11:57:34 +0530

    feat: Create a method to return multiple students

--- MyApp/src/main/java/lk/ac/vau/fas/it/controller/AppController.java
+++ MyApp/src/main/java/lk/ac/vau/fas/it/controller/AppController.java
@@ -1,5 +1,8 @@
 package lk.ac.vau.fas.it.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +16,10 @@ import lk.ac.vau.fas.it.model.Student;
 public class AppController {
 	
 	Student Bob= new Student("2020/ict/44","Bob",24,"IT",3.2);
+	Student Ann= new Student("2020/ict/45","Ann",24,"IT",3.1);
+	Student Suu= new Student("2020/ict/46","Suu",24,"IT",3.0);
+	
+	List<Student> students=new ArrayList<Student>();
 	
 	@GetMapping("/msg")
 	public String myMessage() {
@@ -29,6 +36,13 @@ public class AppController {
 	@GetMapping("/student")
 	public Student getStudent() {
 		return Bob;
+	}
+	@GetMapping("/students")
+	public List<Student> getStudents(){
+		students.add(Bob);
+		students.add(Ann);
+		students.add(Suu);
+		return students;
 	}
 
 }
